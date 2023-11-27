9dd5812338a096d62d41419f0bf1bada294dadb7
Joowon Lim <m4rinesnow34@gmail.com>
2023-11-27 10:48:07 +0900

    Feat: SOLAPI 알림톡 발송 연결

--- src/main/java/com/readyvery/readyverydemo/src/order/OrderServiceImpl.java
+++ src/main/java/com/readyvery/readyverydemo/src/order/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package com.readyvery.readyverydemo.src.order;
 
 import java.nio.charset.StandardCharsets;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,6 +15,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import net.minidev.json.JSONObject;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.model.KakaoOption;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 
 import com.readyvery.readyverydemo.domain.CeoInfo;
 import com.readyvery.readyverydemo.domain.Order;
@@ -21,6 +28,7 @@ import com.readyvery.readyverydemo.domain.repository.CeoRepository;
 import com.readyvery.readyverydemo.domain.repository.OrderRepository;
 import com.readyvery.readyverydemo.global.exception.BusinessLogicException;
 import com.readyvery.readyverydemo.global.exception.ExceptionCode;
+import com.readyvery.readyverydemo.src.order.config.SolApiConfig;
 import com.readyvery.readyverydemo.src.order.config.TossPaymentConfig;
 import com.readyvery.readyverydemo.src.order.dto.OrderMapper;
 import com.readyvery.readyverydemo.src.order.dto.OrderRegisterRes;
@@ -35,11 +43,11 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-
 	private final OrderRepository orderRepository;
 	private final OrderMapper orderMapper;
 	private final CeoRepository ceoRepository;
 	private final TossPaymentConfig tosspaymentConfig;
+	private final SolApiConfig solApiConfig;
 
 	@Override
 	public OrderRegisterRes getOrders(Long id, Progress progress) {
@@ -61,14 +69,49 @@ public class OrderServiceImpl implements OrderService {
 	public OrderStatusRes completeOrder(Long id, OrderStatusUpdateReq request) {
 		CeoInfo ceoInfo = getCeoInfo(id);
 		Order order = getOrder(request.getOrderId());
-
 		verifyPostOrder(ceoInfo, order);
 		verifyPostProgress(order, request);
 		order.completeOrder(request.getStatus());
 		orderRepository.save(order);
+
+		sendCompleteMessage(order, request.getStatus());
 		return OrderStatusRes.builder()
 			.success(true)
 			.build();
+	}
+
+	private void sendCompleteMessage(Order order, Progress status) {
+		if (status == Progress.MAKE) {
+			KakaoOption kakaoOption = makeKakaoOption(solApiConfig.getTempleteOrder());
+			HashMap<String, String> variables = makeMakeVariables(order);
+			sendMessage(kakaoOption, variables, order.getUserInfo().getPhone());
+		}
+		if (status == Progress.COMPLETE) {
+			KakaoOption kakaoOption = makeKakaoOption(solApiConfig.getTempletePickup());
+			HashMap<String, String> variables2 = makeCompeleteVariables(order);
+			sendMessage(kakaoOption, variables2, order.getUserInfo().getPhone());
+		}
+	}
+
+	private HashMap<String, String> makeCompeleteVariables(Order order) {
+		HashMap<String, String> variables = new HashMap<>();
+		variables.put("#{storeName}", order.getStore().getName());
+		variables.put("#{orderName}", order.getOrderName());
+		variables.put("#{orderNumber}", order.getOrderId());
+		variables.put("#{orderId}", order.getOrderId());
+		return variables;
+	}
+
+	private HashMap<String, String> makeMakeVariables(Order order) {
+		HashMap<String, String> variables = new HashMap<>();
+		variables.put("#{userName}", order.getUserInfo().getNickName());
+		variables.put("#{orderDate}", order.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")));
+		variables.put("#{orderNumber}", order.getOrderNumber());
+		variables.put("#{storeName}", order.getStore().getName());
+		variables.put("#{orderName}", order.getOrderName());
+		variables.put("#{estimatedTime}", order.getEstimatedTime().format(DateTimeFormatter.ofPattern("HH:mm")));
+		variables.put("#{orderId}", order.getOrderId());
+		return variables;
 	}
 
 	@Override
@@ -80,9 +123,58 @@ public class OrderServiceImpl implements OrderService {
 		verifyPostProgress(order, request);
 		//order.cancelOrder(request.getStatus());
 		orderRepository.save(order);
+
+		// 카카오 알림톡 전송
+		KakaoOption kakaoOption = makeKakaoOption(solApiConfig.getTempleteCancel());
+		HashMap<String, String> variables = makeCancelVariables(order, request.getRejectReason());
+		String phone = order.getUserInfo().getPhone();
+
+		sendMessage(kakaoOption, variables, phone);
 		return OrderStatusRes.builder()
 			.success(true)
 			.build();
+	}
+
+	private KakaoOption makeKakaoOption(String templateId) {
+		KakaoOption kakaoOption = new KakaoOption();
+		kakaoOption.setPfId(solApiConfig.getKakaoPfid());
+		kakaoOption.setTemplateId(templateId);
+		kakaoOption.setDisableSms(true);
+		return kakaoOption;
+	}
+
+	private HashMap<String, String> makeCancelVariables(Order order, String rejectReason) {
+		HashMap<String, String> variables = new HashMap<>();
+		variables.put("#{storeName}", order.getStore().getName());
+		variables.put("#{orderName}", order.getOrderName());
+		variables.put("#{orderNumber}", order.getOrderId());
+		variables.put("#{cancelReason}", rejectReason);
+		return variables;
+	}
+
+	private void sendMessage(KakaoOption kakaoOption, HashMap<String, String> variables, String phone) {
+		DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(
+			solApiConfig.getApiKey(),
+			solApiConfig.getApiSecret(),
+			SolApiConfig.SOLAPI_URL);
+
+		kakaoOption.setVariables(variables);
+
+		// Message 패키지가 중복될 경우 net.nurigo.sdk.message.model.Message로 치환하여 주세요
+		Message message = new Message();
+		message.setTo(phone);
+		message.setKakaoOptions(kakaoOption);
+
+		try {
+			// send 메소드로 ArrayList<Message> 객체를 넣어도 동작합니다!
+			messageService.send(message);
+		} catch (NurigoMessageNotReceivedException exception) {
+			// 발송에 실패한 메시지 목록을 확인할 수 있습니다!
+			System.out.println(exception.getFailedMessageList());
+			System.out.println(exception.getMessage());
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
 	}
 
 	@Override
@@ -100,17 +192,13 @@ public class OrderServiceImpl implements OrderService {
 				throw new BusinessLogicException(ExceptionCode.NOT_FOUND_TIME);
 			}
 			order.orderTime(request.getTime());
-			return;
 		} else if (order.getProgress() == Progress.ORDER && request.getStatus() == Progress.CANCEL) {
 			if (request.getRejectReason() == null) {
 				throw new BusinessLogicException(ExceptionCode.NOT_FOUND_REJECT_REASON);
 			}
 			cancelTossPayment(order, request);
-			return;
 		} else if (order.getProgress() == Progress.MAKE && request.getStatus() == Progress.COMPLETE) { // 제조 -> 완료
-			return;
 		} else if (order.getProgress() == Progress.COMPLETE && request.getStatus() == Progress.PICKUP) { // 완료 -> 픽업
-			return;
 		} else {
 			throw new BusinessLogicException(ExceptionCode.NOT_CHANGE_ORDER);
 		}
