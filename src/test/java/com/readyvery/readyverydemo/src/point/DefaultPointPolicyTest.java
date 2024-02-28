036a8113bdfcf942cce7d83097b9752616e0d43a
Joowon Lim <m4rinesnow34@gmail.com>
2024-02-28 18:08:39 +0900

    Test: 작은, 중간, 큰 금액 포인트 계산 테스트

--- src/test/java/com/readyvery/readyverydemo/src/point/DefaultPointPolicyTest.java
+++ src/test/java/com/readyvery/readyverydemo/src/point/DefaultPointPolicyTest.java
@@ -2,6 +2,8 @@ package com.readyvery.readyverydemo.src.point;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,12 +15,34 @@ class DefaultPointPolicyTest {
 	private DefaultPointPolicy defaultPointPolicy;
 
 	@Test
-	void calculatePoint() {
+	@DisplayName("작은 금액 테스트")
+	void calculateSmallPoint() {
 		// given
-		Long amount = 10000L; // 계산 금액
+		Long amount = 1_050L; // 계산 금액
+		// when
+		Long point = defaultPointPolicy.calculatePoint(amount);
+		// then
+		assertEquals(26L, point);
+	}
+	@Test
+	@DisplayName("보통 금액 테스트")
+	void calculateNomalPoint() {
+		// given
+		Long amount = 10_000L; // 계산 금액
 		// when
 		Long point = defaultPointPolicy.calculatePoint(amount);
 		// then
 		assertEquals(250L, point);
+	}
+
+	@Test
+	@DisplayName("큰 금액 테스트")
+	void calculateBigPoint() {
+		// given
+		Long amount = 532_400L; // 계산 금액
+		// when
+		Long point = defaultPointPolicy.calculatePoint(amount);
+		// then
+		assertEquals(13_310L, point);
 	}
 }
