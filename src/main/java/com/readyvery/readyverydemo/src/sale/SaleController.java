97183190671b0b874971028c18b19de20b377d65
1223v <1223v@naver.com>
2024-03-06 18:29:47 +0900

    Feat: 총 매출, 주문건수 API 추가

--- src/main/java/com/readyvery/readyverydemo/src/sale/SaleController.java
+++ src/main/java/com/readyvery/readyverydemo/src/sale/SaleController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.readyvery.readyverydemo.security.jwt.dto.CustomUserDetails;
 import com.readyvery.readyverydemo.src.sale.dto.SaleManagementReq;
 import com.readyvery.readyverydemo.src.sale.dto.SaleManagementRes;
+import com.readyvery.readyverydemo.src.sale.dto.SaleManagementTotalMoneyReq;
+import com.readyvery.readyverydemo.src.sale.dto.SaleManagementTotalMoneyRes;
+import com.readyvery.readyverydemo.src.sale.dto.SaleManagementTotalOrderReq;
+import com.readyvery.readyverydemo.src.sale.dto.SaleManagementTotalOrderRes;
 import com.readyvery.readyverydemo.src.sale.dto.TotalSaleRes;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -43,6 +47,35 @@ public class SaleController {
 		return saleServiceImpl.getSaleManagementMoney(userDetails.getId(), request);
 	}
 
+	@Operation(summary = "월간, 주간 총 주문 건수 기능", description = "월간, 주간 총 주문 건수를 출력합니다.", tags = {"매출"})
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "OK"),
+	})
+	@PostMapping("/sale/management/order/total")
+	public SaleManagementTotalOrderRes getSaleManagementOrder(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody SaleManagementTotalOrderReq saleManagementTotalOrderReq) {
+		return saleServiceImpl.getSaleManagementOrder(userDetails.getId(), saleManagementTotalOrderReq);
+	}
 
+	@Operation(summary = "월간 총 매출 기능", description = "월간 총 매출을 출력합니다.", tags = {"매출"})
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "OK"),
+	})
+	@PostMapping("/sale/management/amount/monthly")
+	public SaleManagementTotalMoneyRes getMonthlySalesAmount(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody SaleManagementTotalMoneyReq saleManagementTotalMoneyReq) {
+		return saleServiceImpl.getMonthlySalesAmount(userDetails.getId(), saleManagementTotalMoneyReq);
+	}
+
+	@Operation(summary = "주간 총 매출 기능", description = "월 총 매출을 출력합니다.", tags = {"매출"})
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "OK"),
+	})
+	@PostMapping("/sale/management/amount/weekly")
+	public SaleManagementTotalMoneyRes getWeekSaleManagementMoney(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestBody SaleManagementTotalMoneyReq saleManagementTotalMoneyReq) {
+		return saleServiceImpl.getWeekSaleManagementMoney(userDetails.getId(), saleManagementTotalMoneyReq);
+	}
 
 }
