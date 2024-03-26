784e9ad49a82e2e4128ef21c57bef57d2fdf1e02
Joowon Lim <m4rinesnow34@gmail.com>
2024-03-26 20:03:16 +0900

    Fix: 중복 취소 허용 해결

--- src/main/java/com/readyvery/readyverydemo/domain/repository/OrderRepository.java
+++ src/main/java/com/readyvery/readyverydemo/domain/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,12 +13,15 @@ import com.readyvery.readyverydemo.domain.Order;
 import com.readyvery.readyverydemo.domain.Progress;
 import com.readyvery.readyverydemo.src.sale.dto.SaleManagementDto;
 
+import jakarta.persistence.LockModeType;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	List<Order> findAllById(Long id);
 
 	List<Order> findAllByProgressAndStoreIdAndCreatedAtBetween(
 		Progress progress, Long storeId, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<Order> findByOrderId(String orderId);
 
 	@Query("SELECT SUM(o.totalAmount) FROM Order o WHERE o.store.id = :storeId AND o.estimatedTime IS NOT NULL")
