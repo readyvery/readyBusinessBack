978e07c91680b6da4528affc1ce78f83f2ac2c0f
1223v <1223v@naver.com>
2024-07-16 03:32:00 +0900

    Fix: OrderRegisterRes @AllArgsConstructor 추가

--- src/main/java/com/readyvery/readyverydemo/src/order/dto/OrderRegisterRes.java
+++ src/main/java/com/readyvery/readyverydemo/src/order/dto/OrderRegisterRes.java
@@ -2,11 +2,13 @@ package com.readyvery.readyverydemo.src.order.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class OrderRegisterRes {
 	private Long storeIdx;
 	private List<OrderDto> orders;
